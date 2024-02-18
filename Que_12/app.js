79813ae95f748a9e36dbaa44679b6e65ca49fa63
faizakhirad840 <pc14779.ammar@gmail.com>
2024-02-18 03:18:21 -0800

    Adding Que 12

---
+++ Que_12/app.js
@@ -0,0 +1,13 @@
+var placesToVisit = ["China", "Turkey", "Saudia Arabia", "France", "Italy"];
+console.log(placesToVisit);
+// Sorting places without changing the original array
+var alphabeticalOrderArray = placesToVisit.slice().sort();
+//console.log(placesToVisit)
+//console.log(alphabeticalOrderArray)
+// Sorting in reverse alphabetical order without changing the array
+var reverseAlphabeticalOrder = placesToVisit.slice().sort().reverse();
+//console.log(placesToVisit)
+//console.log(alphabeticalOrderArray)
+// reverse the order of the array
+console.log(placesToVisit.reverse());
+console.log(placesToVisit.reverse());
---
+++ Que_12/app.ts
@@ -0,0 +1,32 @@
+let placesToVisit : string[] = ["China" , "Turkey" ,"Saudia Arabia" , "France" ,"Italy"];
+
+console.log(placesToVisit)
+
+// Sorting places without changing the original array
+
+let alphabeticalOrderArray  = placesToVisit.slice().sort()
+
+//console.log(placesToVisit)
+//console.log(alphabeticalOrderArray)
+
+// Sorting in reverse alphabetical order without changing the array
+
+let reverseAlphabeticalOrder = placesToVisit.slice().sort().reverse()
+
+//console.log(placesToVisit)
+//console.log(alphabeticalOrderArray)
+
+// reverse the order of the array
+
+console.log(placesToVisit.reverse())
+console.log(placesToVisit.reverse())
+
+
+// sorting array
+
+console.log(placesToVisit.sort())
+
+// sorting array in reverse alphabetical order
+
+console.log(placesToVisit.sort().reverse())
+
