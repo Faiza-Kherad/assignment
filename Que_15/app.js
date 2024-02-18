95f98a8456f80ae74f9ab05ecd2cc70d85e28eba
faizakhirad840 <pc14779.ammar@gmail.com>
2024-02-18 04:06:23 -0800

    Adding Que 15

---
+++ Que_15/app.js
@@ -0,0 +1,8 @@
+var geographicalFeatures = {
+    plain: "Gangetic Plain",
+    desert: "Sahara Desert",
+    sea: "Caspian sea",
+    wetland: "West Siberian Lowland",
+    canals: "Manchester Ship Canal"
+};
+console.log(geographicalFeatures);
---
+++ Que_15/app.ts
@@ -0,0 +1,9 @@
+let geographicalFeatures = {
+    plain : "Gangetic Plain",
+    desert : "Sahara Desert",
+    sea : "Caspian sea",
+    wetland: "West Siberian Lowland",
+    canals : "Manchester Ship Canal"
+}
+
+console.log(geographicalFeatures)
