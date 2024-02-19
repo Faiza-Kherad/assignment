e7af68d0248baa697a9840520010c13fa9fcb04b
faizakhirad840 <pc14779.ammar@gmail.com>
2024-02-19 08:39:41 -0800

    Adding Que 30

---
+++ Que_30/app.js
@@ -0,0 +1,4 @@
+function makeTshirt(size, message) {
+    console.log("The size of T-shirt is ".concat(size, " and the message is ").concat(message));
+}
+makeTshirt(35, "Be Brave");
---
+++ Que_30/app.ts
@@ -0,0 +1,5 @@
+function makeTshirt(size : number , message : string){
+    console.log(`The size of T-shirt is ${size} and the message is ${message}`)
+}
+
+makeTshirt(35 , "Be Brave")
