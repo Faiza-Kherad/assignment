3e4b40b952abdb3d336f8b8fb5d5eb1968ab7051
faizakhirad840 <pc14779.ammar@gmail.com>
2024-02-19 08:19:06 -0800

    Adding Que 29

---
+++ Que_29/app.ts
@@ -0,0 +1,13 @@
+let animals = ["cat" , "dog" , "hamster"]
+
+for (const animal of animals) {
+    console.log(animal);
+    
+}
+
+for (const animal of animals) {
+    console.log(`A ${animal} would make a great pet`);
+}
+
+
+console.log("Any of these animals would make a great pet!")
