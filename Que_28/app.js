211245dfcc1b7f6bb1009d7ae86e7c1d51cfc274
faizakhirad840 <pc14779.ammar@gmail.com>
2024-02-19 07:57:35 -0800

    Adding Que 28

---
+++ Que_28/app.js
@@ -0,0 +1,9 @@
+var favouritePizzas = ["Pepperoni", "Margherita", "BBQ Chicken"];
+for (var _i = 0, favouritePizzas_1 = favouritePizzas; _i < favouritePizzas_1.length; _i++) {
+    var favouritePizza = favouritePizzas_1[_i];
+    console.log(" I like ".concat(favouritePizza, " pizza."));
+}
+console.log("Pepperoni is my absolute favorite; its savory and spicy flavor is irresistible!");
+console.log("Margherita, with its simple combination of fresh tomatoes, mozzarella, and basil, is a timeless classic that I always enjoy.");
+console.log("BBQ Chicken pizza is a delightful fusion of tangy barbecue sauce and tender chicken, making it a unique and flavorful choice.");
+console.log("I truly love pizza - the diverse flavors, endless toppings, and the joy it brings to every meal!");
---
+++ Que_28/app.ts
@@ -0,0 +1,10 @@
+let favouritePizzas = ["Pepperoni", "Margherita", "BBQ Chicken" ]
+
+for (const favouritePizza of favouritePizzas) {
+    console.log(`I like ${favouritePizza} pizza.`)
+}
+
+console.log("Pepperoni is my absolute favorite; its savory and spicy flavor is irresistible!")
+console.log("Margherita, with its simple combination of fresh tomatoes, mozzarella, and basil, is a timeless classic that I always enjoy.")
+console.log("BBQ Chicken pizza is a delightful fusion of tangy barbecue sauce and tender chicken, making it a unique and flavorful choice.")
+console.log("I truly love pizza - the diverse flavors, endless toppings, and the joy it brings to every meal!")
