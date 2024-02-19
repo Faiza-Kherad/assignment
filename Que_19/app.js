00342185365d17e1302b8afcfb242a69f4c3c0f3
faizakhirad840 <pc14779.ammar@gmail.com>
2024-02-19 02:56:37 -0800

    Adding Que19

---
+++ Que_19/app.js
@@ -0,0 +1,4 @@
+var alien_color = "yellow";
+if (alien_color == "green") {
+    console.log("The player just earned 5 points");
+}
---
+++ Que_19/app.ts
@@ -0,0 +1,8 @@
+const alien_color : string = "green";
+
+/* The If statement will print the line if the color is green if it's not 
+then there will no output */
+
+if(alien_color == "green"){
+    console.log("The player just earned 5 points")
+}
