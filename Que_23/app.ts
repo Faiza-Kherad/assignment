17b37ecdaf5cb80a2cb4d3a0050b39e15e2017e4
faizakhirad840 <pc14779.ammar@gmail.com>
2024-02-19 04:16:39 -0800

    Adding Que23

---
+++ Que_23/app.ts
@@ -0,0 +1,17 @@
+let favoriteFruits  : string[]= ["Mango" , "Strawberry" , "cherry"]
+
+if(favoriteFruits.includes("Mango")){
+    console.log("You really like Mangoes!")
+}
+if(favoriteFruits.includes("Strawberry")){
+    console.log("You really like strawberries !")
+}
+if(favoriteFruits.includes("cherry")){
+    console.log("You really like cherries !")
+}
+if(favoriteFruits.includes("banana")){
+    console.log("You really like bananas!")
+}
+if(favoriteFruits.includes("orange")){
+    console.log("You really like oranges!")
+}
