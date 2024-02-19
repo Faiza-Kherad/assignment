1b371b0d26b826ece1329f28936fb87c8831ef2e
faizakhirad840 <pc14779.ammar@gmail.com>
2024-02-19 03:31:24 -0800

    Adding Que21

---
+++ Que_21/app.js
@@ -0,0 +1,10 @@
+var alien_color = "red";
+if (alien_color == "green") {
+    console.log("The player earned 5 points");
+}
+else if (alien_color == "yellow") {
+    console.log("The player earned 10 points");
+}
+else if (alien_color == "red") {
+    console.log("The player earned 15 points");
+}
---
+++ Que_21/app.ts
@@ -0,0 +1,41 @@
+// run if block
+
+let alien_color : string = "green";
+
+if(alien_color == "green"){
+    console.log("The player earned 5 points")
+}
+else if(alien_color == "yellow"){
+    console.log("The player earned 10 points")
+}
+else if(alien_color == "red"){
+    console.log("The player earned 15 points")
+}
+
+// run first else if block
+
+ alien_color = "yellow";
+
+if(alien_color == "green"){
+    console.log("The player earned 5 points")
+}
+else if(alien_color == "yellow"){
+    console.log("The player earned 10 points")
+}
+else if(alien_color == "red"){
+    console.log("The player earned 15 points")
+}
+
+// run second else if block
+
+alien_color = "red";
+
+if(alien_color == "green"){
+    console.log("The player earned 5 points")
+}
+else if(alien_color == "yellow"){
+    console.log("The player earned 10 points")
+}
+else if(alien_color == "red"){
+    console.log("The player earned 15 points")
+}
