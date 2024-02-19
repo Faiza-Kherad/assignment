3e1b534e507ed1d1ae0dd62bac30b1ec5f1dc613
faizakhirad840 <pc14779.ammar@gmail.com>
2024-02-19 03:10:58 -0800

    Adding Que 20

---
+++ Que_20/app.js
@@ -0,0 +1,16 @@
+// Runs the IF block
+var alien_color = "green";
+if (alien_color == "green") {
+    console.log("The player just earned 5 points for shooting the alien.");
+}
+else {
+    console.log("The player just earned 10 points.");
+}
+// Runs the IF block
+alien_color = "Red";
+if (alien_color == "green") {
+    console.log("The player just earned 5 points for shooting the alien.");
+}
+else {
+    console.log("The player just earned 10 points.");
+}
---
+++ Que_20/app.ts
@@ -0,0 +1,21 @@
+// Runs the IF block
+
+let alien_color : string = "green";
+
+
+if(alien_color == "green"){
+    console.log("The player just earned 5 points for shooting the alien.")
+}
+else{
+    console.log("The player just earned 10 points.")
+}
+
+// Runs the IF block
+alien_color  = "Red"
+
+if(alien_color == "green"){
+    console.log("The player just earned 5 points for shooting the alien.")
+}
+else{
+    console.log("The player just earned 10 points.")
+}
